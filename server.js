57faf0e408ef7f6e1f905bc7fa69e743ba584a92
Prince Sachdeva <scriptforprince@gmail.com>
2025-03-19 17:21:50 +0530

    Server is running smoothly

--- server.js
+++ server.js
@@ -41,6 +41,9 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', orderRoutes);
 app.use('/api', authRoutes);
+app.get("/", (req, res) => {
+  res.send("Server is running.");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Node server running on port ${PORT}`));
